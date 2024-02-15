7612965036c2c39166b4df8dff331b51748a1bd4
joaoogp <joaoogp@al.insper.edu.br>
2024-02-15 10:49:15 -0300

    parte 4

--- utils.py
+++ utils.py
@@ -17,6 +17,26 @@ def load_data(arquivo):
         return dicionario
     
 def load_template(arquivo):
-    with open("template/" + arquivo, 'r') as arquivo_json:
+    with open("templates/" + arquivo, 'r') as arquivo_json:
         texto = arquivo_json.read()
     
+    return texto
+
+def save_note(params):
+    with open('data/notes.json', 'r') as notes_read:
+        texto = notes_read.read()
+        list = json.loads(texto)
+        list.append(params)
+        novo_json = json.dumps(list)
+
+    with open('data/notes.json', 'w') as notes_arq:
+        notes_arq.write(novo_json)
+
+    
+def build_response(body='', code=200, reason='OK', headers=''):
+    if headers == '':
+        resposta = f'HTTP/1.1 {code} {reason}\n\n {body}'
+    else:
+        resposta = f'HTTP/1.1 {code} {reason}\n {headers}\n\n {body}'
+    
+    return resposta.encode()
